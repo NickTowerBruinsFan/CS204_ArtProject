c6bd617bcbcbb17076165eba72bb38de19f12d52
Owlss <eerinmacdonaldd@gmail.com>
2023-01-17 18:16:01 -0500

    add gitgnore
    added gitgnore

--- ArtPackage204/MainWindow.xaml.cs
+++ ArtPackage204/MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace ArtPackage204
         {
             InitializeComponent();
         }
+
+        int picture = 1;
+        private void maingrid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {            
+            
+                var point = Mouse.GetPosition(maingrid);
+
+                int row = 0;
+                int col = 0;
+                double accumulatedHeight = 0.0;
+                double accumulatedWidth = 0.0;
+
+                // calc row mouse was over
+                foreach (var rowDefinition in maingrid.RowDefinitions)
+                {
+                    accumulatedHeight += rowDefinition.ActualHeight;
+                    if (accumulatedHeight >= point.Y)
+                        break;
+                    row++;
+                }
+
+                // calc col mouse was over
+                foreach (var columnDefinition in maingrid.ColumnDefinitions)
+                {
+                    accumulatedWidth += columnDefinition.ActualWidth;
+                    if (accumulatedWidth >= point.X)
+                        break;
+                    col++;
+                }
+            
+
+            var img = new Image { Width = 60, Height = 60 };
+            var bitmapImage = new BitmapImage();
+            if (picture == 0)
+            {
+                bitmapImage = new BitmapImage(new Uri(@"C:\Users\eerin\OneDrive\Pictures\grass.jpg"));
+            } 
+            else if (picture == 1)
+            {
+                bitmapImage = new BitmapImage(new Uri(@"C:\Users\eerin\OneDrive\Pictures\uppath.jpg"));
+            }
+            img.Source = bitmapImage;
+
+            img.SetValue(Grid.RowProperty, row);
+            img.SetValue(Grid.ColumnProperty, col);
+
+            maingrid.Children.Add(img);
+        }
+
+        private void Button_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Grass selected.");
+            picture = 0;
+        }
+
+        private void Button_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Road selected.");
+            picture = 1;
+        }
     }
 }
