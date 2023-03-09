1fb99d6d71f73c75151db25ab4b38ba680f4f25b
Owlss <eerinmacdonaldd@gmail.com>
2023-03-09 17:38:26 -0500

    bug fix
    fixes bugs

--- ArtPackage204/Page2.xaml.cs
+++ ArtPackage204/Page2.xaml.cs
@@ -292,14 +292,30 @@ namespace ArtPackage204
 
         BitmapImage[] GetImages()
         {
-            string currDir = Directory.GetCurrentDirectory();
-            string[] imageUris;
-
-            //Get directory path of myData 
-            string temp = currDir + "\\Images\\";
-            imageUris = Directory.GetFiles(temp, "*.jpg");
+            var imageUris = new[]
+            {
+ "pack://application:,,,/Images/crosspath.jpg",
+ "pack://application:,,,/Images/curvepath.jpg",
+ "pack://application:,,,/Images/curvepathbottemleft.jpg",
+ "pack://application:,,,/Images/curvepathupperleft.jpg",
+ "pack://application:,,,/Images/curvepathupperright.jpg",
+ "pack://application:,,,/Images/door.jpg",
+ "pack://application:,,,/Images/door1.jpg",
+ "pack://application:,,,/Images/earser.jpg",
+ "pack://application:,,,/Images/grass.jpg",
+ "pack://application:,,,/Images/horixontalpath.jpg",
+ "pack://application:,,,/Images/stairs.jpg",
+ "pack://application:,,,/Images/stone floor.jpg",
+ "pack://application:,,,/Images/tshapepath.jpg",
+ "pack://application:,,,/Images/tshapepath2.jpg",
+ "pack://application:,,,/Images/tshapepath3.jpg",
+ "pack://application:,,,/Images/tshapepath4.jpg",
+ "pack://application:,,,/Images/uppath.jpg",
+ "pack://application:,,,/Images/wall.jpg",
+ "pack://application:,,,/Images/window.jpg",
+ "pack://application:,,,/Images/window1.jpg"
+ };
             return imageUris.Select(CreateBitmap).ToArray();
-
         }
 
         private void ListBoxItem_PreviewMouseLeftButtonDown_20(object sender, MouseButtonEventArgs e)
